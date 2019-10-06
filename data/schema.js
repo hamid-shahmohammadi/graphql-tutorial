a2ea312b8edb8fe480978de33b8a0cf2d01c830e
hamid-shahmohammadi <hamid@shahmohamadi@gmail.com>
2019-10-06 13:52:12 +0330

    lesson-5-1

--- data/schema.js
+++ data/schema.js
@@ -1,6 +1,7 @@
-import { buildSchema } from 'graphql'
+import { resolvers } from './resolvers';
+import { makeExecutableSchema } from 'graphql-tools';
 
-const schema=buildSchema(`
+const typeDefs=`
     type Friend{
         id:ID
         firstName:String
@@ -41,6 +42,9 @@ const schema=buildSchema(`
     type Mutation{
         createFriend(input:FriendInput):Friend
     }
-`)
 
-export default schema;
+    
+`;
+const schema=makeExecutableSchema({ typeDefs, resolvers });
+
+export { schema  };
